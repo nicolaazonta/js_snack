720880e2e177dd3c224832958533c033b4a48b39
nicolaazonta <nicolazonta114@gmail.com>
2023-03-15 12:45:54 +0100

    add cycle while option

--- jssnack5/main.js
+++ jssnack5/main.js
@@ -5,7 +5,7 @@ Chiedi per 6 volte all’utente di inserire un numero,
 se è dispari inseriscilo nell’array. */
 /* let numbers = []; ------------with for cycle
 
-for (let i = 0; i < 6; i++) {
+for (let i = 0; i < 5; i++) {
 
     const attempt = Number(prompt('type a number'));
 
@@ -19,14 +19,17 @@ console.log(numbers); */
 
 
 
-
-for (let i = 0; i < 6; i++) {
+let numbers = [];
+let k = 0;
+while (k < 5) {
 
     const attempt = Number(prompt('type a number'));
 
     if ((attempt % 2) != 0){
         numbers.push(attempt)
     }
+
+    k++
     
 }
 
